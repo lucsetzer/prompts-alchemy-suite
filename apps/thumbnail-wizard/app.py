80c5c9e35d7d85a134b06d3c845de7dfdeea55d2
Luc Setzer <lucsetzer@gmail.com>
2026-01-03 09:14:59 -0500

    Fix import path for sys module

--- apps/thumbnail-wizard/app.py
+++ apps/thumbnail-wizard/app.py
@@ -9,7 +9,7 @@ import io
 import json
 import requests
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from layout import layout
 from dotenv import load_dotenv
 
